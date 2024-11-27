33b5ef98e8628f351bebc3aec802cf39250f5789
lucyimhoff <lucyi4@nycstudents.net>
2024-11-27 19:31:15 +0000

    updated: added three new methods

--- Algorithms.java
+++ Algorithms.java
@@ -10,9 +10,13 @@ public class Algorithms {
         int odds = odds();
         int evens = evens();
         int twoDigits = twoDigits();
+        int aboveFiveHundred = aboveFiveHundred();
+        int greatest = greatest();
         System.out.println("odds: " + odds);
         System.out.println("evens: " + evens);
         System.out.println("two digits: " + twoDigits);
+        System.out.println("above five hundred: " + aboveFiveHundred);
+        System.out.println("greatest number: " + greatest);
         s.close();
     }
 
@@ -41,9 +45,33 @@ public class Algorithms {
         s = new Scanner(f);
         int twoDigits = 0;
         while (s.hasNext()) {
-            //if (s.nextInt() % 10 >= 0) figure out how to determine two digits
+            if (s.nextInt() / 10 >= 0 && s.nextInt() < 10)
                 twoDigits++;
         }
         return twoDigits;
     }
+
+    public static int aboveFiveHundred() throws FileNotFoundException{
+        s = new Scanner(f);
+        int aboveFiveHundred = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() > 500)
+            aboveFiveHundred++;
+        }
+        return aboveFiveHundred;
+    }
+
+    public static int greatest() throws FileNotFoundException{
+        s = new Scanner(f);
+        int greatest = s.nextInt();
+        while (s.hasNext()) {
+            int current = s.nextInt();
+            if(current > greatest)
+            greatest = current;
+            
+            
+        }
+        return greatest;
+    }
+
 }
